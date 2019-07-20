bb48145aafa08c0fb2c3d4ee0c3e0fab77f8627d
Qibin Chen <chenqibin422@gmail.com>
2019-07-20 17:39:41 +0800

    Update

---
+++ process.sh
@@ -0,0 +1,4 @@
+mv uploads/1/2/6/5/126578848/background-images/background-images/* uploads/1/2/6/5/126578848/background-images
+mv files/theme/files/* files/theme
+find *.html -type f -exec sed -i '.bak' 's/https\:\/\/ajax\.googleapis\.com\/ajax\/libs\/jquery\/1\.8\.3\/jquery\.min\.js/https\:\/\/cdnjs\.cloudflare\.com\/ajax\/libs\/jquery\/1\.8\.3\/jquery\.min\.js/g' {} +
+find *.bak -delete
